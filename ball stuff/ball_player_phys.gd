b11bd726f2377d5c6ac95f5cce6067e5578fe8d8
DISN-kolo <arrtemsky@gmail.com>
2023-03-10 19:47:05 +0100

    added rank snapping colors and changed the theme

--- ball stuff/ball_player_phys.gd
+++ ball stuff/ball_player_phys.gd
@@ -3,13 +3,45 @@ extends RigidBody3D
 var times_jumped = 0 
 var jump_ready = false
 signal out_of_the_bounds
-var camera_normal :Vector3 = Vector3.ZERO
+var camera_normal : Vector3 = Vector3.ZERO
+var Mat : ShaderMaterial
+var current_color : Vector3 = Vector3(0, 0.2, 0.5)
+var prev_color : Vector3 = Vector3(0, 0.2, 0.5)
+var rank : int = 0
+var prev_rank : int = 0
+var interp_coeff : float = 0
 
+@export var color_norank : Color = Color(0, 0.16, 0.43)
+@export var color_C : Color = Color(0, 0.84, 0.69)
+@export var color_B : Color = Color(0.93, 0.01, 0)
+@export var color_A : Color = Color(1, 0.85, 0)
+#var vector_norank : Vector3
+#var vector_C : Vector3
+#var vector_B : Vector3
+#var vector_A : Vector3
+var color_dict : Array
+
+func color_to_vector_rgb(color_input: Color) -> Vector3:
+	return Vector3(color_input.r, color_input.g, color_input.b)
 
 func _ready():
-	Events.connect("ball_is_out_of_bounds", restart_position) 
+	Events.connect("ball_is_out_of_bounds", restart_position)
+	Mat = get_node("CSGSphere3D").get_material()
+	
+	color_dict = [
+		color_to_vector_rgb(color_norank),
+		color_to_vector_rgb(color_C),
+		color_to_vector_rgb(color_B),
+		color_to_vector_rgb(color_A) ]
 
 func _process(delta):
+	if prev_rank != rank: # rank was changed
+		interp_coeff = 0
+		prev_color = current_color
+	if interp_coeff < 1: # lerp from prev color to new with X seconds (delta/X)
+		interp_coeff += delta/0.1
+		current_color = prev_color.lerp(color_dict[rank], interp_coeff)
+	Mat.set_shader_parameter("colour", current_color)
 	Events.rotations_for_camera = linear_velocity.x
 	Events.speed_for_export = linear_velocity.z
 	if times_jumped == 0:
@@ -29,8 +61,18 @@ func _process(delta):
 		times_jumped += 1 
 	
 	
-	Events.positions = str(int(self.position.x)) + "___"+ str(int(self.position.y)) + "___" + str(int(self.position.z)) 
-
+#	Events.positions = str(int(self.position.x)) + "___"+ str(int(self.position.y)) + "___" + str(int(self.position.z)) 
+	Events.positions = "%d___%d___%d" % [position.x, position.y, position.z]
+	
+	if Events.score_rise_to_style != null:
+		if Events.score_rise_to_style < 5:
+			rank = 0
+		if Events.score_rise_to_style >5 and Events.score_rise_to_style <10 :
+			rank = 1
+		if Events.score_rise_to_style >10 and Events.score_rise_to_style <20 :
+			rank = 2
+		if Events.score_rise_to_style >20 and Events.score_rise_to_style <40 :
+			rank = 3
 
 func _on_body_entered(body):
 	times_jumped = 0
